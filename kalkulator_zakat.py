6aee741d18550924fd509f2b9d274cef4d06f22f
Nada Fadhiilah Balqis <106371427+nadqz@users.noreply.github.com>
2023-12-05 17:53:34 +0700

    Update kalkulator_zakat.py

--- kalkulator_zakat.py
+++ kalkulator_zakat.py
@@ -3,23 +3,23 @@ import streamlit as st
 def zakat_penghasilan():
     st.title("Kalkulator Zakat Penghasilan")
     st.write("---")
-    st.write("Zakat penghasilan atau zakat profesi adalah zakat yang wajib dibayarkan atas pendapatan yang diperoleh dari pekerjaan atau profesi, sepeti gaji. Zakat penghasilan termasuk dalam kategori zakat mal, yang merupakan kewajiban membayar zakat karena memiliki atau menyimpan harta seperti uang dan emas.")
+    st.write("Zakat penghasilan atau zakat profesi adalah zakat yang wajib dibayarkan atas pendapatan yang diperoleh dari pekerjaan atau profesi, seperti gaji. Zakat penghasilan termasuk dalam kategori zakat mal, yang merupakan kewajiban membayar zakat karena memiliki atau menyimpan harta seperti uang dan emas.")
 
-    #Input Penghasilan per Bulan
+    # Input Penghasilan per Bulan
     penghasilan_bulanan = st.number_input("Penghasilan per Bulan (IDR)")
 
-    #Input Penghasilan Lainnya per Bulan
+    # Input Penghasilan Lainnya per Bulan
     penghasilan_lainnya = st.number_input("Penghasilan Lainnya per Bulan (IDR)")
 
-    #Input Hutang atau Cicilan
+    # Input Hutang atau Cicilan
     hutang_cicilan = st.number_input("Hutang atau Cicilan per Bulan (IDR)")
 
-    #Menghitung Jumlah Penghasilan Setelah Dikurangi Hutang atau Cicilan
+    # Menghitung Jumlah Penghasilan Setelah Dikurangi Hutang atau Cicilan
     penghasilan_setelah_hutang = penghasilan_bulanan + penghasilan_lainnya - hutang_cicilan
 
-    #Menampilkan Jumlah Penghasilan yang Telah Dikalkulasi
-    st.write("Jumlah Penghasilan Bersih per Bulan: ", penghasilan_setelah_hutang, "IDR")
-    
+    # Menampilkan Jumlah Penghasilan yang Telah Dikalkulasi
+    st.write("Jumlah Penghasilan Bersih per Bulan: IDR {:,.2f}".format(penghasilan_setelah_hutang))
+
     st.title("Nisab Zakat Penghasilan")
     st.write("Nisab merupakan batas minimum jumlah harta yang harus terpenuhi agar dapat dianggap sebagai harta yang wajib dizakati. Zakat hanya diberlakukan pada penghasilan yang melampaui nisab tersebut. Untuk Zakat Penghasilan, nisab yang ditetapkan setara dengan 85 gram emas.")
     st.header("Niat Zakat Penghasilan")
@@ -27,27 +27,27 @@ def zakat_penghasilan():
     st.caption("Nawaitu an ukhrija zakatadz maali fardhan lillahi ta’ala.")
     st.write("Artinya: Aku niat mengeluarkan zakat hartaku fardhu karena Allah Ta'ala.")
 
-    #Input Harga Emas Saat Ini per Gram
+    # Input Harga Emas Saat Ini per Gram
     harga_emas = st.number_input("Harga Emas Saat Ini per Gram (IDR)")
 
-    #Menentukan Apakah Harga Emas Bernilai 0
-    if harga_emas == 0:
-        st.write("Harga Emas Tidak Boleh Bernilai 0")
+    # Menentukan Apakah Harga Emas Bernilai 0
+    if harga_emas <= 0:
+        st.error("Harga Emas harus lebih dari 0")
         return
     
-    #Menghitung Besaran Nishab Zakat Penghasilan per Bulan
+    # Menghitung Besaran Nisab Zakat Penghasilan per Bulan
     nisab_penghasilan = (85 / 12 * harga_emas)
-    st.write("Besaran Nishab Zakat Penghasilan per Bulan: ", nisab_penghasilan, "IDR")
+    st.write("Besaran Nishab Zakat Penghasilan per Bulan: IDR {:,.2f}".format(nisab_penghasilan))
 
-    #Memeriksa Penghasilan Setelah Hutang lebih besar daripada Nisab
+    # Memeriksa Penghasilan Setelah Hutang lebih besar daripada Nisab
     if penghasilan_setelah_hutang >= nisab_penghasilan:
         st.write("Penghasilan Anda melebihi Nisab untuk Zakat Penghasilan.")
 
-    # Hitung Zakat
+        # Hitung Zakat
         zakat_amount = 0.025 * penghasilan_setelah_hutang
-        st.write("Jumlah Zakat Penghasilan yang Harus Dibayarkan: ", zakat_amount, "IDR")
+        st.write("Jumlah Zakat Penghasilan yang Harus Dibayarkan: IDR {:,.2f}".format(zakat_amount))
     else:
         st.write("Penghasilan Anda belum mencapai Nisab untuk Zakat Penghasilan.")
-        
+
 # Run the function
 zakat_penghasilan()
